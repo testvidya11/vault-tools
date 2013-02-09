b7e403a1f7732e29e337adbba5b17fb2d446c3fd
Jamu Kakar <jkakar@kakar.ca>
2013-02-09 12:31:50 -0800

    - New Vault::App.hid_to_id and Vault::User.hid_to_id convert Heroku   app and user IDs into core app and user IDs.

--- test/app_test.rb
+++ test/app_test.rb
@@ -14,6 +14,21 @@ class AppTest < Vault::TestCase
     assert_equal(uuid, Vault::App.id_to_uuid(1234))
   end
 
+  # App.hid_to_id converts a Heroku app ID into a core integer app ID.
+  def test_hid_to_id
+    assert_equal(1234, Vault::App.hid_to_id('app1234@heroku.com'))
+  end
+
+  # App.hid_to_id raises an ArgumentError if the specified ID doesn't match
+  # the expected format.
+  def test_hid_to_id_with_invalid_heroku_id
+    error = assert_raises(ArgumentError) do
+      Vault::App.hid_to_id('invalid1234@heroku.com')
+    end
+    assert_equal('invalid1234@heroku.com is not a valid Heroku app ID.',
+                 error.message)
+  end
+
   # App.hid_to_uuid converts a Heroku app ID to a v5 UUID based on a URL
   # scheme.
   def test_hid_to_uuid
@@ -22,7 +37,7 @@ class AppTest < Vault::TestCase
     assert_equal(uuid, Vault::App.hid_to_uuid('app1234@heroku.com'))
   end
 
-  # App.hid_to_uuid raises a ArgumentError if the specified ID doesn't match
+  # App.hid_to_uuid raises an ArgumentError if the specified ID doesn't match
   # the expected format.
   def test_hid_to_uuid_with_invalid_heroku_id
     error = assert_raises(ArgumentError) do
