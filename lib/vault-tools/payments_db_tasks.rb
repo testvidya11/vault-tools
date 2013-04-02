99acbbd72da7cc613ea9ef969be1e1c1ff5a8ba5
Chris Continanza <christopher.continanza@gmail.com>
2013-04-02 16:43:58 -0700

    use structure

--- lib/vault-tools/payments_db_tasks.rb
+++ lib/vault-tools/payments_db_tasks.rb
@@ -7,7 +7,7 @@ task :pull_payments do
   end
   steps << 'git clone -n git@github.com:heroku/payments --depth 1'
   steps << 'cd payments'
-  steps << 'git checkout HEAD db/development_structure.sql'
+  steps << 'git checkout HEAD db/structure.sql'
   # make sure we don't submodule it
   steps << 'rm -rf .git'
   sh steps.join(' && ')
@@ -16,7 +16,7 @@ end
 desc "Drop and create vault-usage-test, payments-test and shushu-test databases"
 task :create_payments_db => [:drop_payments_db, :pull_payments] do
   sh 'createdb payments-test'
-  sh 'psql payments-test -f contrib/payments/db/development_structure.sql'
+  sh 'psql payments-test -f contrib/payments/db/structure.sql'
 end
 
 desc "Drop the vault-usage-test, payments-test and shushu-test databases"
